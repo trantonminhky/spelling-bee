dcc8d90c18db91b331cbdcd845998ded01108b45
trantonminhky <fungusvalentinealt69420@gmail.com>
2025-03-29 00:07:39 +0700

    implement

---
+++ index.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const say = require("say");
+
+// parsing
+const wordList = fs.readFileSync("./input.txt", { encoding: "utf-8" });
+const currentEncounter = fs.readFileSync("./repeated.txt", { encoding: "utf-8" });
+const wordListArray = wordList.split('\n');
+const currentEncounterArray = currentEncounter.split('\n');
+
+// picking
+let rand = Math.floor(Math.random() * wordListArray.length);
+let randomWord = wordListArray[rand];
+
+while (currentEncounterArray.includes(randomWord)) {;
+	rand = Math.floor(Math.random() * wordListArray.length);
+	randomWord = wordListArray[rand];
+}
+
+// parsing
+say.getInstalledVoices((err, h) => {
+	console.log(h);
+});
+console.log(randomWord);
+say.export(`Your next word is... ${randomWord}`, "Microsoft David Desktop", 0.85, "test.wav", err => {
+	if (err) console.error(err);
+});
+
+// reinsert
+currentEncounterArray.push(randomWord);
+fs.writeFileSync("./repeated.txt", currentEncounterArray.join('\n'));
