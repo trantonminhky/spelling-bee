6f6de2d4f3a818f4b05da6bb450876031af9b3e0
trantonminhky <fungusvalentinealt69420@gmail.com>
2025-03-29 14:52:59 +0700

    added speed dial

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const say = require("say");
 // parsing
 const wordList = fs.readFileSync("./input.txt", { encoding: "utf-8" });
 const currentEncounter = fs.readFileSync("./repeated.txt", { encoding: "utf-8" });
+const speed = fs.readFileSync("./speed.txt", { encoding: "utf-8" });
 const wordListArray = wordList.split('\n');
 const currentEncounterArray = currentEncounter.split('\n');
+const speedNum = parseInt(speed);
+if (isNaN(speedNum)) return console.log("Invalid speed");
 
 // picking
 let rand = Math.floor(Math.random() * wordListArray.length);
@@ -21,12 +24,13 @@ say.getInstalledVoices((err, h) => {
 	console.log(h);
 });
 console.log(randomWord);
-/*
-say.export(`Your next word is... ${randomWord}`, "Microsoft David Desktop", 0.85, "test.wav", err => {
+
+/* uncomment this to make it export instead
+say.export(`Your next word is... ${randomWord}`, "Microsoft David Desktop", speedNum, "test.wav", err => {
 	if (err) console.error(err);
 });
 */
-say.speak(`Your next word is... ${randomWord}`, "Microsoft David Desktop", 0.85);
+say.speak(`Your next word is... ${randomWord}`, "Microsoft David Desktop", speedNum);
 
 // reinsert
 currentEncounterArray.push(randomWord);
